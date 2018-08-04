10e018413b971aa3f754e2016b46b9e1446f0e66
Jonathan Pelletier <funmungus@gmail.com>
2018-08-04 00:46:20 -0400

    Avoid reading non-grounded pins
    Use regular Serial interface by default.

--- DigitalSlave.ino
+++ DigitalSlave.ino
@@ -1,14 +1,16 @@
+/* Avoid reading non-grounded pins */
+#define SLAVE_BUTTON_COUNT 7
+
 /* Digital pin numbers to read from */
 int _pins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 14, 15, 16};
 /* Current pin state, always same array length as _pins */
-int _states[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-size_t _pins_len = sizeof(_pins) / sizeof(int);
+int _states[SLAVE_BUTTON_COUNT];
 int _current_value = 0;
 
 void setup() {
   /* Slave writes serial to master. */
-  Serial1.begin(9600);
-  for (int i = 0; i < _pins_len; i++) {
+  Serial.begin(9600);
+  for (int i = 0; i < SLAVE_BUTTON_COUNT; i++) {
     pinMode(_pins[i], INPUT);
   }
 }
@@ -19,7 +21,7 @@ void loop() {
   int curBit = 1;
   size_t i = 0;
   /* Iterate pins to read */
-  for (; i < _pins_len; i++) {
+  for (; i < SLAVE_BUTTON_COUNT; i++) {
     state = digitalRead(_pins[i]);
     if (state != _states[i]) {
       if (!hasChanged)
@@ -35,6 +37,6 @@ void loop() {
   }
   /* If a value has changed, write bit values to master. */
   if (hasChanged)
-    Serial1.write((char *)(&_current_value), sizeof(_current_value));
+    Serial.write((char *)(&_current_value), sizeof(_current_value));
   delay(50);
 }
